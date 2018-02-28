68363c81f8e2a5246e0e71d5720813ed77e34fa3
kalivid <13264589472@163.com>
2018-02-28 21:38:50 +0800

    深科技页面和文章详细

--- src/router.config.js
+++ src/router.config.js
@@ -4,6 +4,7 @@ import Micro from './components/micro.vue'
 import News from './components/news.vue'
 import Login from './components/login.vue'
 import CourseDeatil from './components/courseDetail.vue'
+import NewsDetail from './components/newsDetail.vue'
 
 
 export default{
@@ -14,6 +15,7 @@ export default{
     {path:'/news', component:News},
     {path:'/login', component:Login},
     {path:'/coursedetail/:id/', component:CourseDeatil},
+    {path:'/news/detail/:nid/', component:NewsDetail},
   ],
   mode: 'history'  // 去掉url中的#号
 }
